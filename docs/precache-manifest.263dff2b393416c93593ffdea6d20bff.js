48bc8ec43f6db5ec4d06487133b25b418d52a49c
Cristofer Garcia <cristofergarcia840@gmail.com>
2021-10-28 15:14:33 -0600

    Cambio a docs

---
+++ docs/precache-manifest.263dff2b393416c93593ffdea6d20bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b593338bbf01ee452f6a3a7679858c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84e65b79324963abba07",
+    "url": "/static/css/main.333c7821.chunk.css"
+  },
+  {
+    "revision": "935749a595a243032b82",
+    "url": "/static/js/2.664731a6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.664731a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e65b79324963abba07",
+    "url": "/static/js/main.56df4bbf.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
